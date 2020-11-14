1a36b833b4b30b702a18d7dbd03921378ca92313
Jordon123 <46872554+Jordon123@users.noreply.github.com>
2020-11-13 17:46:28 -0800

    Added zoom in and out

---
+++ core/src/com/lsystem/Camera.java
@@ -0,0 +1,64 @@
+package com.lsystem;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+
+public class Camera implements InputProcessor {
+    static boolean forward = false;
+    static boolean back = false;
+    @Override
+    public boolean keyDown(int keycode) {
+        switch(keycode){
+            case(Input.Keys.W):{
+                forward = true;
+            }
+            case(Input.Keys.S):{
+                back = true;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        switch(keycode){
+            case(Input.Keys.W):{
+                    forward = false;
+            }
+            case(Input.Keys.S):{
+                    back = false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+}
--- core/src/com/lsystem/Game.java
+++ core/src/com/lsystem/Game.java
@@ -2,35 +2,60 @@ package com.lsystem;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	ShapeRenderer sr;
 	FractalTree ft;
+	OrthographicCamera camera;
+	Viewport viewport;
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		sr = new ShapeRenderer();
 		ft = new FractalTree();
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
 
 	@Override
-	public void render () {
+	public void render() {
+		camera.update();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		String tree = ft.setup(3);
-		ft.draw(tree,sr);
+		String tree = ft.setup(8);
+
+		if(Gdx.input.isKeyPressed(Input.Keys.W)) {
+			camera.zoom += 2;
+		}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.S)) {
+			camera.zoom -= 2;
+		}
+		sr.setProjectionMatrix(camera.combined);
+		ft.draw(tree, sr);
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 		img.dispose();
+	}
+
+	public void mouseInput() {
+		Gdx.input.getX();
+		Gdx.input.getY();
+
 	}
 }
