c43f44b02729eaea3a7b3b8bcf087e4c925b9e51
Jordon123 <46872554+Jordon123@users.noreply.github.com>
2020-11-20 19:52:37 -0800

    WIP: 3d fractal

--- core/src/com/lsystem/FractalTree3D.java
+++ core/src/com/lsystem/FractalTree3D.java
@@ -9,11 +9,9 @@ import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.MeshBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Vector;
-import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Vector3;
 
-import static com.badlogic.gdx.math.MathUtils.round;
 import static com.badlogic.gdx.math.MathUtils.sinDeg;
 import static com.badlogic.gdx.math.MathUtils.cosDeg;
 
@@ -22,77 +20,88 @@ import java.util.Stack;
 
 //https://en.wikipedia.org/wiki/L-system
 public class FractalTree3D {
-    /* variables: 0, 1
-       constants: [, ]
+    /* variables: A, B
+       constants: [, ] , / , /, F, "
         axiom: 0
         rules:
-            1 -> 11
-            0 -> 1[0]0
+            A -> [B] //// [B] //// [B]
+            B -> &FFFA
      */
     public String generate(String str){
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < str.length(); i++){
             char c = str.charAt(i);
-            if(c == '1'){
-                sb.append("11");
+            if(c == 'A'){
+                sb.append("\"[&FFFA]////[&FFFA]////[&FFFA]");
             }
-            else if(c == '0'){
-                sb.append("1[0]0");
-            }
-            else if(c == '['){
-                sb.append('[');
-            }
-            else if(c == ']'){
-                sb.append(']');
+            else{
+                sb.append(c);
             }
         }
         return sb.toString();
     }
     public String setup(int iterations){
-        String tree = "0";
+        String tree = "A";
         for(int i = 0; i < iterations; i++){
             tree = generate(tree);
         }
         return tree;
     }
+    public void rotateX(float angle, Vector3 points){
+
+    }
     public ModelInstance draw(String str, MeshBuilder meshBuilder, ModelBuilder modelBuilder){
-        Stack<ArrayList<Integer>> stack = new Stack<>();
+        Stack<ArrayList<Float>> stack = new Stack<>();
         //start location
-        int x = 0;
-        int y = 0;
-        int angle = 90;
-        int lineLength = 1;
+        float x = 0;
+        float y = 0;
+        float z = 0;
+        Vector3 point = new Vector3();
+        Vector3 point2 = new Vector3();
+        //float angle = 90;
+        float pitch = 0;
+        float roll = 90;
+        float lineLength = 2;
+        float ratio = (float) 0.5;
         meshBuilder.begin(VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal, GL20.GL_LINES);
         for(int i = 0; i < str.length(); i++){
             char c = str.charAt(i);
-            if(c == '1'){
-                int x2 = round(x + lineLength*cosDeg(angle));
-                int y2 = round(y + lineLength*sinDeg(angle));
-                meshBuilder.line(x, y, 0, x2, y2,0);
+            if(c == 'F'){
+                float x2 = x + lineLength*(cosDeg(roll));
+                float y2 = y + lineLength*(sinDeg(pitch));
+                float z2 = z + lineLength*(-sinDeg(roll)+cosDeg(pitch));
+                meshBuilder.line(x, y, z, x2, y2, z2);
                 x = x2;
                 y = y2;
+                z = z2;
             }
-            else if(c == '0'){
-                int x2 = round(x + lineLength*cosDeg(angle));
-                int y2 = round(y + lineLength*sinDeg(angle));
-                meshBuilder.line(x, y, 0, x2, y2,0);
-                x = x2;
-                y = y2;
+            else if(c == '"'){
+                lineLength = 1 * ratio;
+            }
+            else if(c == '/'){
+                roll = roll + 15;
+            }
+            else if (c == '&'){
+                pitch = pitch + 45;
             }
             else if(c == '['){
-                ArrayList<Integer> posNAngle = new ArrayList<>();
+                ArrayList<Float> posNAngle = new ArrayList<>();
                 posNAngle.add(x);
                 posNAngle.add(y);
-                posNAngle.add(angle);
+                posNAngle.add(z);
+                posNAngle.add(roll);
+                posNAngle.add(lineLength);
+                posNAngle.add(pitch);
                 stack.push(posNAngle);
-                angle = angle + 45;
             }
             else if(c == ']'){
-                ArrayList<Integer> posNAngle = stack.pop();
+                ArrayList<Float> posNAngle = stack.pop();
                 x = posNAngle.get(0);
                 y = posNAngle.get(1);
-                angle = posNAngle.get(2);
-                angle = angle - 45;
+                z = posNAngle.get(2);
+                roll = posNAngle.get(3);
+                lineLength = posNAngle.get(4);
+                pitch = posNAngle.get(5);
             }
         }
         Mesh m = meshBuilder.end();
